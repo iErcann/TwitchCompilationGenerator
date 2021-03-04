e39aa23aff7a6900d05d86eed500add106e0b9ba
iercan <iercan@gmail.com>
2021-03-04 04:45:46 +0100

    GUI

--- gui/src/components/Search.vue
+++ gui/src/components/Search.vue
@@ -7,39 +7,31 @@
             class="ma-1"
             width="50%"
             color="accent"
-            elevation="24"
             outlined
           > Search </v-btn>
         </v-card-actions>
 
-        <v-container class="mt-1">
+        <v-container class="mt-5">
           <v-row class="justify-center">
-            <template v-for="n in 200">
-              <v-sheet
+            <template v-for="n in 100">
+              <v-card
                 :key="n"
-                class="ma-4"
-                outlined
+                class="ma-5"
                 width="250"
-                height="250"
               >
-                <v-sheet
-                  class="pa-3 primary text-right"
-                  dark
-                  rounded="t-xl"
-                  height="50"
-                >
-                  <v-btn
-                    class="ml-2"
-                    icon
-                  >
-                    <v-icon>mdi-check-bold</v-icon>
-                  </v-btn>
-                </v-sheet>
-                <v-img src="https://clips-media-assets2.twitch.tv/41865006814-offset-11888-preview-480x272.jpg">
 
-                </v-img>
-                Clip {{ n }}
-              </v-sheet>
+                <v-card-title>
+                  <v-btn>
+                    INFO
+                  </v-btn>
+                  <v-btn>
+                    LINK
+                  </v-btn>
+                </v-card-title>
+                  <v-img src="https://clips-media-assets2.twitch.tv/AT-cm%7C1070286972-preview-480x272.jpg" />
+                  
+
+              </v-card>
             </template>
           </v-row>
         </v-container>
@@ -49,8 +41,9 @@
     <div>
       <v-bottom-navigation
         fixed
+        outlined
         min-width="300px"
-        background-color="primary"
+        background-color="accent"
         style=" 
         border-radius: 20px;
         left: 50%;
@@ -60,14 +53,14 @@
       >
         <v-btn
           style="width: 100%; height:100%;"
-          color="primary"
+          color="accent"
         >
           <span>ADD SELECTED CLIPS</span>
           <v-icon>mdi-table-plus </v-icon>
         </v-btn>
         <v-btn
           style="width: 100%; height:100%;"
-          color="primary"
+          color="accent"
         >
           <span> EDIT </span>
           <v-icon> mdi-arrow-right-bold-box-outline</v-icon>
@@ -81,10 +74,20 @@
 .border {
   border-radius: 450px;
 }
+
+#fixedContainer {
+  position: fixed;
+  width: 600px;
+  height: 200px;
+  right: 50%;
+  margin-left: 300px; /*half the width*/
+}
 </style>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Search extends Vue {}
+export default class Search extends Vue {
+  checkbox = true;
+}
 </script>
